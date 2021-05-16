189354d7a3459c40cb7e3cb10377323cb5b60d71
anver <anver@anver-pc>
2021-05-16 04:15:13 -0500

    Se agrega librerias, archivo xml y modificacion de la clase Programa

--- src/mx/uam/is/practicadiseno/Programa.java
+++ src/mx/uam/is/practicadiseno/Programa.java
@@ -1,9 +1,6 @@
 package mx.uam.is.practicadiseno;
 
-import mx.uam.is.practicadiseno.datos.MapeadorDatos;
-import mx.uam.is.practicadiseno.datos.MapeadorDatosImpl;
-import mx.uam.is.practicadiseno.negocio.ManejadorImpl;
-import mx.uam.is.practicadiseno.vista.Ventana;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Programa {
 
@@ -12,21 +9,8 @@ public class Programa {
 	 */
 	public static void main(String[] args) {
 
-		// Crea el mapedor de datos
-		MapeadorDatos mapeador = new MapeadorDatosImpl();
+		// Carga el application context
+		new ClassPathXmlApplicationContext("mx/uam/is/practicadiseno/applicationContext.xml");
 
-		// Crea el manejador
-		ManejadorImpl manejador = new ManejadorImpl(mapeador);
-
-		// Creamos las tres ventanas
-		Ventana ventana1 = new Ventana(manejador);
-		Ventana ventana2 = new Ventana(manejador);
-		Ventana ventana3 = new Ventana(manejador);
-
-		// Muestra las ventanas arrancando asi el programa
-		ventana1.setVisible(true);
-		ventana2.setVisible(true);
-		ventana3.setVisible(true);
 	}
-
 }
